f5f91df24bcd95b341b341e28fb100094944060c
Lucas Trindade Silva <38840406+lucastrindadesilva@users.noreply.github.com>
2022-10-10 00:28:10 -0300

    Update palindrome_counter.py

--- Algorithms/Palindromic Substrings Counter/palindrome_counter.py
+++ Algorithms/Palindromic Substrings Counter/palindrome_counter.py
@@ -16,9 +16,9 @@ def palindrome_counter(text : str) -> int:
     
     result = len(text)
     for pos in range(len(text)): #small notation = O(n * n/2) | big notation = O(n^2)
-        if len(text) > pos + 1:
+        if len(text) > pos + 1: #even case
             result += recursive_palindrome_counter(text=text, idx = pos, idy = pos+1)
-        if pos - 1 >= 0 and len(text) > pos + 1:
+        if pos - 1 >= 0 and len(text) > pos + 1: #odd case
             result += recursive_palindrome_counter(text=text, idx = pos-1, idy = pos+1)
     return result
 
@@ -27,12 +27,7 @@ def recursive_palindrome_counter(text : str, idx : int, idy : int) -> int:
         return recursive_palindrome_counter(text=text, idx=idx-1, idy=idy+1) + 1
     return 0
 
-res = palindrome_counter('aaa')
-print(res)
-res = palindrome_counter('abc')
-print(res)
-res = palindrome_counter('abcba')
-print(res)
-res = palindrome_counter('aaaa')
-print(res)
-    
+assert palindrome_counter('aaa') == 6, "Should be 6"
+assert palindrome_counter('abc') == 3, "Should be 3"
+assert palindrome_counter('abcba') == 7, "Should be 7"
+assert palindrome_counter('aaaa') == 10, "Should be 10"    
