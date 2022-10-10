4c387701a733ad355fa9421aca0b9f4580a80827
Lucas Trindade Silva <38840406+lucastrindadesilva@users.noreply.github.com>
2022-10-10 13:56:30 -0300

    Update heap.py
    Max Heap Implemented

--- Data Structures/Binary Heap/heap.py
+++ Data Structures/Binary Heap/heap.py
@@ -5,8 +5,8 @@
    Implementação da heap binária
 """
 
-class MinHeap:
-   """Binary Minimum Heap Class"""
+class Heap:
+   """Binary Heap Class"""
    def __init__(self) -> None:
       self.heap = []
       self.lenght = 0
@@ -31,7 +31,16 @@ class MinHeap:
             return position // 2
       else:
          return False
+               
+   def __str__(self) -> str:
+      str_heap = [str(h) for h in self.heap] 
+      return '|'.join(str_heap)
    
+class MinHeap(Heap):
+   """Binary Minimum Heap Class"""
+   def __init__(self) -> None:
+      super().__init__()
+      
    def insert(self, value : int) -> None: #O(NlogN)
       ### insere o novo valor no final do heap
       self.heap.append(value)
@@ -69,36 +78,107 @@ class MinHeap:
          else:
             break
       ### retorna o valor que estava no topo
-      return result
-               
-   def __str__(self) -> str:
-      str_heap = [str(h) for h in self.heap] 
-      return '|'.join(str_heap)
-         
-heap = MinHeap()
+      return result    
+   
+class MaxHeap(Heap):
+   """Binary Maximum Heap Class"""
+   def __init__(self) -> None:
+      super().__init__()
+      
+   def insert(self, value : int) -> None: #O(NlogN)
+      ### insere o novo valor no final do heap
+      self.heap.append(value)
+      self.lenght += 1
+      ### reordena o heap para que o novo elemento do final assuma sua nova posição
+      if self.lenght > 1:
+         position = self.lenght - 1
+         while(self.get_father(position) is not False
+               and self.heap[self.get_father(position)] < self.heap[position]):
+            swap = self.heap[self.get_father(position)]
+            self.heap[self.get_father(position)] = self.heap[position]
+            self.heap[position] = swap
+            position = self.get_father(position)
+            
+   def remove(self) -> int:
+      ### remove o elemento que está no topo - armazena o valor para retornar
+      result = self.heap[0]
+      ### o último elemento se torna o primeiro
+      self.heap[0] = self.heap[-1]
+      del(self.heap[-1])
+      self.lenght -= 1
+      ### reordena o heap para que o novo elemento do topo assuma sua nova posição
+      position = 0
+      while True:
+         if self.get_left(position) is not False and self.heap[self.get_left(position)] > self.heap[position]:
+            swap = self.heap[position]
+            self.heap[position] = self.heap[self.get_left(position)]
+            self.heap[self.get_left(position)] = swap
+            position = self.get_left(position)
+         elif self.get_right(position) is not False and self.heap[self.get_right(position)] > self.heap[position]:
+            swap = self.heap[position]
+            self.heap[position] = self.heap[self.get_right(position)]
+            self.heap[self.get_right(position)] = swap
+            position = self.get_right(position)
+         else:
+            break
+      ### retorna o valor que estava no topo
+      return result   
+        
+############## tests cases         
+heap = MinHeap()   
 
-print("Inserting 2 to heap")
+print("Inserting 2 to Minheap")
 heap.insert(2)
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
-print("Inserting 7 to heap")
+print("Inserting 7 to Minheap")
 heap.insert(7)
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
-print("Inserting 26 to heap")
+print("Inserting 26 to Minheap")
 heap.insert(26)
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
-print("Inserting 25 to heap")
+print("Inserting 25 to Minheap")
 heap.insert(25)
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
-print("Inserting 19 to heap")
+print("Inserting 19 to Minheap")
 heap.insert(19)
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
 print("removed: {}".format(heap.remove()))
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
 
 print("removed: {}".format(heap.remove()))
-print("Heap = {}".format(heap))
+print("MinHeap = {}".format(heap))
+
+#######
+
+heap = MaxHeap()   
+
+print("Inserting 2 to Maxheap")
+heap.insert(2)
+print("MaxHeap = {}".format(heap))
+
+print("Inserting 7 to Maxheap")
+heap.insert(7)
+print("MaxHeap = {}".format(heap))
+
+print("Inserting 26 to Maxheap")
+heap.insert(26)
+print("MaxHeap = {}".format(heap))
+
+print("Inserting 25 to Maxheap")
+heap.insert(25)
+print("MaxHeap = {}".format(heap))
+
+print("Inserting 19 to Maxheap")
+heap.insert(19)
+print("MaxHeap = {}".format(heap))
+
+print("removed: {}".format(heap.remove()))
+print("MaxHeap = {}".format(heap))
+
+print("removed: {}".format(heap.remove()))
+print("MaxHeap = {}".format(heap))
